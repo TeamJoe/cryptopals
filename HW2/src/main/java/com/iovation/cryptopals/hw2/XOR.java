5b484ed60055bf3024cc0ee7b57d91402e2c2100
Joseph Salomone <joseph.salomone@iovation.com>
2020-02-12 15:07:49 -0800

    HW2

---
+++ HW2/src/main/java/com/iovation/cryptopals/hw2/XOR.java
@@ -0,0 +1,41 @@
+package com.iovation.cryptopals.hw2;
+
+public class XOR {
+    public static String xor(String value, String xor) {
+        return toHex(xor(toBin(value), toBin(xor))).substring(0, value.length());
+    }
+
+    public static byte[] xor(byte[] value, byte[] xor) {
+        var result = new byte[value.length];
+        for(int i = 0; i < value.length; i++) {
+            result[i] = (byte)(value[i] ^ xor[i]);
+        }
+        return result;
+    }
+
+    private static byte[] toBin(String hex) {
+        var value = new byte[(hex.length() / 2) + (hex.length() % 2)];
+        for(int i = 0; i < hex.length(); i += 2) {
+            value[i/2] = (byte)((getBinaryFromHex(hex.charAt(i)) << 4) + ((i+1) < hex.length() ? getBinaryFromHex(hex.charAt(i+1)) : 0));
+        }
+        return value;
+    }
+
+    private static String toHex(byte[] value) {
+        var result = "";
+
+        for(int i = 0; i < value.length; i++) {
+            result += getHexFromBinary(value[i]);
+        }
+
+        return result;
+    }
+
+    private static byte getBinaryFromHex(char hex) {
+        return (byte)Character.digit(hex, 16);
+    }
+
+    private static String getHexFromBinary(byte hex) {
+        return Integer.toHexString((hex & 0xF0) >>> 4) + Integer.toHexString(hex & 0x0F);
+    }
+}
---
+++ HW2/src/main/java/com/iovation/cryptopals/hw2/main.java
@@ -0,0 +1,8 @@
+package com.iovation.cryptopals.hw2;
+
+public class main {
+    public static int main(String args[]) {
+        System.out.println(XOR.xor(args[1], args[2]));
+        return 0;
+    }
+}
